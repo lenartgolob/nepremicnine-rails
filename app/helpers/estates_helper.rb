a6a0c04fee13ce5b49c2d812f8d9af80c2eb1022
lenci123 <lenart.golob17@gmail.com>
2019-12-28 14:38:15 +0100

    yessir

--- app/helpers/estates_helper.rb
+++ app/helpers/estates_helper.rb
@@ -1,13 +1,10 @@
 module EstatesHelper
     def show_estate_buttons(estate)
-        html = '<td>'
-        html += link_to 'Show', estate
-        html += '</td>'
+        html = ''
         if estate.user == current_user
-          html += '<td>'
+          html += '<td id="edit">'
           html += link_to 'Edit', edit_estate_path(estate)
-          html += '</td>'
-          html += '<td>'
+          html += '<br><br>'
           html += link_to 'Destroy', estate, method: :delete, data: { confirm: 'Are you sure?' }
           html += '</td>'
         end
