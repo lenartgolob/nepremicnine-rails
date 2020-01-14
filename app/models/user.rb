bd29d6e20f9bb56b808e339010ef6c24f52bb0ce
lenci123 <lenart.golob17@gmail.com>
2020-01-14 16:53:28 +0100

    fb & google login delata

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :estates, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
-         :omniauthable, omniauth_providers: %i[facebook]
+         :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
 
 
 
@@ -27,5 +27,19 @@ class User < ApplicationRecord
       # user.skip_confirmation!
     end
   end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+end
 
 end
