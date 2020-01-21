ca2ee8f9a0ee9801a827e787875ec98dfa19a9aa
Lenart Golob <lenart.golob17@gmail.com>
2020-01-21 13:04:42 +0100

    active admin dela frend moj

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'omniauth-facebook'
 gem 'omniauth-google-oauth2'
+gem 'activeadmin'
+
 
 
 gem "mini_magick"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :estates
   root to: 'pages#index'
   get '/estates/#/novogradnja', to: 'estates#novogradnja'
